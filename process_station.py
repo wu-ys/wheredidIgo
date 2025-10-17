458926d2e3cd763c5a4e2c43075a5246daa28c0d
wu-ys <wu-ys20@mails.tsinghua.edu.cn>
2025-10-18 00:09:36 +0800

    dataset of railway stations. TODO: fix some stations

---
+++ process_station.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import json
+
+from utils.project import gcj02towgs84
+
+# === 1. 读取 JSON 文件 ===
+with open('assets/station_lnglat_20250217.json', 'r', encoding='utf-8') as f:
+    json_data = json.load(f)
+
+# 构造 name → (lat, lng) 的字典
+coord_dict = {item["name"]: (item["lat"], item["lng"]) for item in json_data}
+
+# === 2. 读取 CSV 文件 ===
+df = pd.read_csv('railway/station.csv', encoding='utf-8')
+
+# # === 3. 匹配经纬度 ===
+# df['lat'] = df['name'].map(lambda x: coord_dict.get(x, (None, None))[0])
+# df['lon'] = df['name'].map(lambda x: coord_dict.get(x, (None, None))[1])
+
+# === 3. 匹配经纬度，并记录未匹配项 ===
+missing_names = []
+records = []
+for name in df['name']:
+    if name in coord_dict:
+        lat, lon = coord_dict[name][0], coord_dict[name][1]
+        lon, lat = gcj02towgs84(lon, lat)
+        records.append({"name": name, "lat": lat, "lon": lon})
+    else:
+        # records.append({"name": name, "lat": None, "lon": None})
+        missing_names.append(name)
+
+# === 4. 转换为 JSON 格式并保存 ===
+wrapped = {"railway": records}
+
+with open('railway/station.json', 'w', encoding='utf-8') as f:
+    json.dump(wrapped, f, ensure_ascii=False, indent=2)
+
+print("✅ 已生成 station.json 文件！")
