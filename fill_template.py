ae95946dde333ede698afc9dae574d907965ffcb
wu-ys <wu-ys20@mails.tsinghua.edu.cn>
2025-12-18 23:08:31 +0800

    rename print_gpx_name to fill_template; add flights page

---
+++ fill_template.py
@@ -0,0 +1,38 @@
+import os
+import glob
+
+if __name__ == "__main__":
+
+    railway_gpx_files = sorted(glob.glob(os.path.join("./railway", "**", "*.gpx"), recursive=True))
+    flights_gpx_files = sorted(glob.glob(os.path.join("./flights", "**", "*.gpx"), recursive=True))
+
+    # load template and write
+    with open("index_template.html", 'r', encoding='utf-8') as file:
+        template = file.read()
+
+    template = template.replace(f"{{railway}}", str(railway_gpx_files))
+    template = template.replace(f"{{flights}}", str(flights_gpx_files))
+    template = template.replace(f"\\\\", "/")
+
+    with open("index.html", 'w', encoding='utf-8') as file:
+        file.write(template)
+
+    # load railway template and write
+    with open("railway_template.html", 'r', encoding='utf-8') as file:
+        template = file.read()
+
+    template = template.replace(f"{{railway}}", str(railway_gpx_files))
+    template = template.replace(f"\\\\", "/")
+    with open("railway.html", 'w', encoding='utf-8') as file:
+        file.write(template)
+
+    # load flights template and write
+    # load railway template and write
+    with open("flights_template.html", 'r', encoding='utf-8') as file:
+        template = file.read()
+
+    template = template.replace(f"{{flights}}", str(flights_gpx_files))
+    template = template.replace(f"{{gpx_num}}", str(len(flights_gpx_files)))
+    template = template.replace(f"\\\\", "/")
+    with open("flights.html", 'w', encoding='utf-8') as file:
+        file.write(template)    
